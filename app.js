2ffd6b1dc91744e3065848819808cfc17e5c0363
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-03-20 22:10:25 -0300

    Aplicando máscara de valores

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const converterMoedaParaBRL = valor =>
     currency: 'BRL'
   })
 
+const aplicarMascaraMoeda = input => {
+  const inputValue = input.value.trim()
+
+  let result = inputValue.toString().replace(/[^\d]+/g, '')
+  result = result.replace(/([\d]{2})$/, ".$1")
+
+  if (result.length > 6) {
+    result = result.replace(/\B(?=(\d{3})+[^\d])/g, "," )
+  }
+
+  input.value = result
+}
+
 const buscarDadosLocalStorage = () => {
   const dadosTransacoes = localStorage.getItem('transacoes')
 
@@ -94,7 +107,7 @@ const adicionarTransacao = event => {
   const transacoes = buscarDadosLocalStorage()
   const form = event.target
   const nome = form.text.value.trim()
-  const valor = form.amount.value.trim()
+  const valor = form.amount.value.trim().replaceAll(',', '')
 
   if (!nome || !valor) {
     console.log(`Preencha corretamente`)
