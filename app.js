79f2300ae864250410a43cdaabd8edd183c0e44a
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-03-26 20:41:49 -0300

    :bug: Resolvido conflito de tipos durante edição de produto

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ const editarTransacao = event => {
 
 const abrirEdicaoDeTransacao = idTransacao => {
   const transacoes = buscarDadosLocalStorage()
-  const transacaoASerEditada = transacoes.find(({ id }) => id === idTransacao )
+  const transacaoASerEditada = transacoes.find(({ id }) => +id === +idTransacao )
 
   const popup = document.querySelector('.popup-wrapper')
       const popupEditar = popup.cloneNode(true)
