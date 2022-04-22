c74e4b51c20b20a1aec663774de301a8aad960ba
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-04-22 16:36:47 -0300

    Adicionado ícone de fundo à lista de transações

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-import { converterDataParaBr, converterMoedaParaBRL, aplicarMascaraMoeda } from "./utils/functions.js";
+import {
+  converterDataParaBr,
+  converterMoedaParaBRL,
+  aplicarMascaraMoeda,
+} from "./utils/functions.js";
 
 const buscarDadosLocalStorage = () =>
- JSON.parse(localStorage.getItem('transacoes'))
+  JSON.parse(localStorage.getItem("transacoes"));
 
-const persistirTransacoesLocalStorage = transacoes => {
-  localStorage.setItem('transacoes', JSON.stringify(transacoes))
-}
+const persistirTransacoesLocalStorage = (transacoes) => {
+  localStorage.setItem("transacoes", JSON.stringify(transacoes));
+};
 
 const exibirTransacao = ({ id, nome, valor, date }) => {
-  const classeASerAplicada = valor < 0 ? 'minus' : 'plus'
-  const li = document.createElement('li')
+  const classeASerAplicada = valor < 0 ? "minus" : "plus";
+  const li = document.createElement("li");
 
-  li.setAttribute('data-id', id)
-  li.classList.add(classeASerAplicada)
+  li.setAttribute("data-id", id);
+  li.classList.add(classeASerAplicada);
 
   const conteudoLi = `
     <div class="date">${converterDataParaBr(new Date(date))}</div> 
@@ -20,195 +24,210 @@ const exibirTransacao = ({ id, nome, valor, date }) => {
     <span class="preco">${converterMoedaParaBRL(Math.abs(valor))}</span>
     <button class="edit-btn" data-edit="${id}"></button>
     <button class="delete-btn" data-delete="${id}"></button>
-  `
-  li.innerHTML = conteudoLi
-  ulTransacoes.prepend(li)
-}
+  `;
+  li.innerHTML = conteudoLi;
+  ulTransacoes.prepend(li);
+};
 
-const calcularDespesas = arrayTransacoes => {
-  const balancoContainer = document.getElementById('balance')
-  const receitasContainer = document.getElementById('money-plus')
-  const despesasContainer = document.getElementById('money-minus')
-  
-  const totalTransacoes = arrayTransacoes
-    .reduce((acc, { valor }) => {
-      const transacao = valor < 0 ? 'despesas' : 'receitas'
-      acc[transacao] = acc[transacao] + Math.abs(valor)
+const calcularDespesas = (arrayTransacoes) => {
+  const balancoContainer = document.getElementById("balance");
+  const receitasContainer = document.getElementById("money-plus");
+  const despesasContainer = document.getElementById("money-minus");
 
-      return acc
-    }, { despesas: 0, receitas: 0 })
+  const totalTransacoes = arrayTransacoes.reduce(
+    (acc, { valor }) => {
+      const transacao = valor < 0 ? "despesas" : "receitas";
+      acc[transacao] = acc[transacao] + Math.abs(valor);
 
-  const { receitas, despesas } = totalTransacoes
-  const saldoAtual = receitas - despesas
+      return acc;
+    },
+    { despesas: 0, receitas: 0 }
+  );
 
-  balancoContainer.style.color = saldoAtual >= 0 ? '#2ecc71' : '#c0392b'
+  const { receitas, despesas } = totalTransacoes;
+  const saldoAtual = receitas - despesas;
 
-  balancoContainer.innerText = converterMoedaParaBRL(saldoAtual)
-  receitasContainer.innerText = `+ ${converterMoedaParaBRL(receitas)}`
-  despesasContainer.innerText = `- ${converterMoedaParaBRL(despesas)}`
-}
+  balancoContainer.style.color = saldoAtual >= 0 ? "#2ecc71" : "#c0392b";
 
-const atualizarInformacoes = transacoes => {
-  calcularDespesas(transacoes)
+  balancoContainer.innerText = converterMoedaParaBRL(saldoAtual);
+  receitasContainer.innerText = `+ ${converterMoedaParaBRL(receitas)}`;
+  despesasContainer.innerText = `- ${converterMoedaParaBRL(despesas)}`;
+};
+
+const atualizarInformacoes = (transacoes) => {
+  calcularDespesas(transacoes);
 
   if (transacoes.length !== 0) {
-    ulTransacoes.innerHTML = ''
-  
-    persistirTransacoesLocalStorage(transacoes)
-    transacoes.forEach(exibirTransacao)
+    ulTransacoes.innerHTML = "";
+
+    persistirTransacoesLocalStorage(transacoes);
+    transacoes.forEach(exibirTransacao);
   }
-}
+};
 
-const editarTransacao = event => {
-  event.preventDefault()
+const editarTransacao = (event) => {
+  event.preventDefault();
 
-  const {text: nome, date, tipo, amount: valor, id} = Object
-    .fromEntries(new FormData(event.target).entries())
-  
-  const transacoes = buscarDadosLocalStorage()
+  const {
+    text: nome,
+    date,
+    tipo,
+    amount: valor,
+    id,
+  } = Object.fromEntries(new FormData(event.target).entries());
 
-  const indexTransacao = transacoes
-        .findIndex(({ id: idAtual }) => idAtual === id)
-  
-  transacoes.splice(indexTransacao, 1,
-    { nome,
-      date,
-      tipo,
-      valor: tipo === 'receita' ? valor : valor * -1,
-      id
-    })
-  
-  atualizarInformacoes(transacoes)
-  event.target.closest('.popup-wrapper').classList.toggle('d-none')
-}
+  const transacoes = buscarDadosLocalStorage();
 
-const abrirEdicaoDeTransacao = idTransacao => {
-  const transacoes = buscarDadosLocalStorage()
-  const transacaoASerEditada = transacoes.find(({ id }) => +id === +idTransacao )
+  const indexTransacao = transacoes.findIndex(
+    ({ id: idAtual }) => idAtual === id
+  );
 
-  const popup = document.querySelector('.popup-wrapper')
-  const popupEditar = popup.cloneNode(true)
+  transacoes.splice(indexTransacao, 1, {
+    nome,
+    date,
+    tipo,
+    valor: tipo === "receita" ? valor : valor * -1,
+    id,
+  });
 
-  popupEditar.querySelector('h3').innerText = 'Editar transação'
-  popupEditar.querySelector('.btn').innerText = 'Editar'
+  atualizarInformacoes(transacoes);
+  event.target.closest(".popup-wrapper").classList.toggle("d-none");
+};
 
-  const idInput = document.createElement('input')
-  idInput.name = 'id'
-  idInput.value = transacaoASerEditada.id
-  idInput.hidden = true
-  
-  const formEditar = popupEditar.querySelector('form')
-  formEditar.appendChild(idInput)
+const abrirEdicaoDeTransacao = (idTransacao) => {
+  const transacoes = buscarDadosLocalStorage();
+  const transacaoASerEditada = transacoes.find(
+    ({ id }) => +id === +idTransacao
+  );
 
-  // console.log(formEditar.amount)
-  formEditar.text.value = transacaoASerEditada.nome
-  formEditar.date.value = transacaoASerEditada.date
-  formEditar.amount.value = Math.abs(transacaoASerEditada.valor)
-  Array.from(formEditar.tipo)
-    .find(input => input.value === transacaoASerEditada.tipo).checked = true
+  const popup = document.querySelector(".popup-wrapper");
+  const popupEditar = popup.cloneNode(true);
 
-  document.body.append(popupEditar)
-    
-  popupEditar.classList.toggle('d-none')
-  popupEditar.querySelector('#inputValue')
-    .addEventListener('keyup', event => aplicarMascaraMoeda(event.target))
-  popupEditar.addEventListener('click', fecharPopup)
-  popupEditar.addEventListener('submit', editarTransacao)
-}
+  popupEditar.querySelector("h3").innerText = "Editar transação";
+  popupEditar.querySelector(".btn").innerText = "Editar";
 
-const excluirTransacao = event => {
-  const elementoClicado = event.target
+  const idInput = document.createElement("input");
+  idInput.name = "id";
+  idInput.value = transacaoASerEditada.id;
+  idInput.hidden = true;
 
-  if (elementoClicado.tagName === 'BUTTON') {
-    const liTransacao = elementoClicado.closest('li')
-    const idTransacao = liTransacao.dataset.id
-    const transacoes = buscarDadosLocalStorage()
+  const formEditar = popupEditar.querySelector("form");
+  formEditar.appendChild(idInput);
+
+  const novaData = new Date(transacaoASerEditada.date)
+    .toISOString()
+    .split("T")[0];
+
+  formEditar.text.value = transacaoASerEditada.nome;
+  formEditar.date.value = novaData;
+  formEditar.amount.value = Math.abs(transacaoASerEditada.valor);
+  Array.from(formEditar.tipo).find(
+    (input) => input.value === transacaoASerEditada.tipo
+  ).checked = true;
+
+  document.body.append(popupEditar);
+
+  popupEditar.classList.toggle("d-none");
+  popupEditar
+    .querySelector("#inputValue")
+    .addEventListener("keyup", (event) => aplicarMascaraMoeda(event.target));
+  popupEditar.addEventListener("click", fecharPopup);
+  popupEditar.addEventListener("submit", editarTransacao);
+};
+
+const excluirTransacao = (event) => {
+  const elementoClicado = event.target;
+
+  if (elementoClicado.tagName === "BUTTON") {
+    const liTransacao = elementoClicado.closest("li");
+    const idTransacao = liTransacao.dataset.id;
+    const transacoes = buscarDadosLocalStorage();
 
     if (elementoClicado.dataset.delete) {
-      const indexTransacao = transacoes
-        .findIndex(({ id }) => id === idTransacao)
-  
-      transacoes.splice(indexTransacao, 1)
-      liTransacao.remove()
-  
-      atualizarInformacoes(transacoes)
-      persistirTransacoesLocalStorage(transacoes)
+      const indexTransacao = transacoes.findIndex(
+        ({ id }) => id === idTransacao
+      );
 
-      return 
+      transacoes.splice(indexTransacao, 1);
+      liTransacao.remove();
+
+      atualizarInformacoes(transacoes);
+      persistirTransacoesLocalStorage(transacoes);
+
+      return;
     }
 
     if (elementoClicado.dataset.edit) {
-      abrirEdicaoDeTransacao(idTransacao)
+      abrirEdicaoDeTransacao(idTransacao);
     }
   }
-}
+};
 
-const gerarId = () => Date.now() + Math.floor(Math.random() * 1000)
+const gerarId = () => Date.now() + Math.floor(Math.random() * 1000);
 
-const adicionarTransacao = event => {
-  event.preventDefault()
+const adicionarTransacao = (event) => {
+  event.preventDefault();
 
-  const transacoes = buscarDadosLocalStorage()
-  const form = event.target
-  const date = new Date(form.date.value)
-  const tipo = form.tipo.value
-  const nome = form.text.value.trim()
-  const valor = form.amount.value.trim().replaceAll(',', '')
+  const transacoes = buscarDadosLocalStorage();
+  const form = event.target;
+  const date = new Date(form.date.value);
+  const tipo = form.tipo.value;
+  const nome = form.text.value.trim();
+  const valor = form.amount.value.trim().replaceAll(",", "");
 
-  if (!nome || !valor) return
+  if (!nome || !valor) return;
 
   const despesa = {
     id: gerarId(),
     nome,
     date,
     tipo,
-    valor: tipo === 'receita' ? valor : valor * -1
-  }
-  
-  transacoes.push(despesa)
-  atualizarInformacoes(transacoes)
+    valor: tipo === "receita" ? valor : valor * -1,
+  };
 
-  form.reset()
-  popupContainer.classList.toggle('d-none')
-}
+  transacoes.push(despesa);
+  atualizarInformacoes(transacoes);
+
+  form.reset();
+  popupContainer.classList.toggle("d-none");
+};
 
 const abrirPopup = () => {
-  const popup = document.querySelector('.popup-wrapper')
-  popup.classList.toggle('d-none')
+  const popup = document.querySelector(".popup-wrapper");
+  popup.classList.toggle("d-none");
+};
+
+function fecharPopup(event) {
+  const elementoClicado = event.target;
+
+  if (elementoClicado.classList.contains("popup-close"))
+    this.classList.toggle("d-none");
 }
 
-function fecharPopup (event) {
-  const elementoClicado = event.target
+const btnAdicionarTransacao = document.getElementById("btn-add-transaction");
+btnAdicionarTransacao.addEventListener("click", abrirPopup);
 
-  if(elementoClicado.classList.contains('popup-close')) 
-    this.classList.toggle('d-none')
-}
+const popupContainer = document.querySelector(".popup-wrapper");
+popupContainer.addEventListener("click", fecharPopup);
 
-const btnAdicionarTransacao = document.getElementById('btn-add-transaction')
-btnAdicionarTransacao.addEventListener('click', abrirPopup)
+const form = document.getElementById("form");
+form.addEventListener("submit", adicionarTransacao);
 
-const popupContainer = document.querySelector('.popup-wrapper')
-popupContainer.addEventListener('click', fecharPopup)
+const ulTransacoes = document.getElementById("transactions");
+ulTransacoes.addEventListener("click", excluirTransacao);
 
-const form = document.getElementById('form')
-form.addEventListener('submit', adicionarTransacao);
-
-const ulTransacoes = document.getElementById('transactions')
-ulTransacoes.addEventListener('click', excluirTransacao)
-
-document.addEventListener('keyup', e => {
+document.addEventListener("keyup", (e) => {
   if (e.target === inputValue) {
-    aplicarMascaraMoeda(e.target)
+    aplicarMascaraMoeda(e.target);
   }
-})
+});
 
 window.onload = () => {
-  const transacoes = localStorage.getItem('transacoes')
-  
-  if(!transacoes) {
-    localStorage.setItem('transacoes', JSON.stringify([]))
-    return
-  }
-  atualizarInformacoes(JSON.parse(transacoes))
-}
+  const transacoes = localStorage.getItem("transacoes");
 
+  if (!transacoes) {
+    localStorage.setItem("transacoes", JSON.stringify([]));
+    return;
+  }
+  atualizarInformacoes(JSON.parse(transacoes));
+};
